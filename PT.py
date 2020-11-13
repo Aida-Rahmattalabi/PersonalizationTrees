b1937e5f9b24afe85df7a1e9f48933d45bbaecbc
Aida-Rahmattalabi <rahmatta@usc.edu>
2020-11-12 17:57:10 -0800

    Update PT.py

--- PT.py
+++ PT.py
@@ -1,17 +1,44 @@
-# This is an implementation of the decision tree of the paper 
+# This is an implementation of Algorithm 1 in the paper 
 # "Recursive Partitioning for Personalization using Observational Data"
 
+import numpy as np
 
-def PT(Data, current_depth, max_depth, min_leaf_number, feature_num):
-	X = Data[0]	  # covariates
-	T = Data[1]	  # treatment
-	Y = Data[2]   # outcome
 
-	d = len(X[0])  # number of features
-	m = max(Y) + 1 # number of actions/treatments
+class node():
+	def __init__(self, l, v):
+		self.l = l
+		self.v = v
 
-	for l in range(d):
+		self.childLeft = None
+		self.childRight = None
+
+
+
+
+class personalizationTree():
+
+	def __init__(self, X, Y, T, max_depth, min_leaf_number):
+		self.X = X
+		self.Y = Y
+		self.Z = Z
+		self.max_depth = max_depth
+		self.min_leaf_number = min_leaf_number
+
+		self.d = len(X[0])  				# number of features
+		self.m = 0
+		self.candidateCuts = sqrt(self.d)
+
 		
+		self.subroutine(0)
 
+
+	def dataProcessing():
+		pass
+		# implement later for data processing
+
+
+	def subroutine(cur_depth)
+		for l in range(d):
+		
 
 
