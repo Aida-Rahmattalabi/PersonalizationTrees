e508391365c195514b9cbf0df11b57bb06a46e92
Aida-Rahmattalabi <rahmatta@usc.edu>
2020-11-19 17:36:44 -0800

    initial commit

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import pandas as pd
-from scipy.stats import multinomial
+from scipy.stats import bernoulli
+from scipy.stats import norm
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import r2_score,mean_squared_error
+import seaborn as sns
 import random 
+import numpy as np
+import time
+import math
 
-
-#-------------------------------------------------------------------------------	
+import tree
+#---------------------------------------------------------------------------------------------------	
 def generate_data_guassian(d, n, m):
 	from sklearn.datasets import make_gaussian_quantiles
 	# d is the number of covariates 
@@ -17,11 +24,8 @@ def generate_data_guassian(d, n, m):
 	X = pd.DataFrame(X,columns=['x'+str(i) for i in range(d)])
 	Y0 = pd.Series(Y)
 
-	# print("X:", X)
-	# print("Labels:", Y)
-	
 	return X, Y0
-#-------------------------------------------------------------------------------	
+#---------------------------------------------------------------------------------------------------	
 def generate_complex_data(d, n, m):
 	from sklearn.datasets import make_classification
 	# d is the number of covariates 
@@ -36,28 +40,178 @@ def generate_complex_data(d, n, m):
 	X = pd.DataFrame(X,columns=['x'+str(i) for i in range(d)])
 	Y0 = pd.Series(Y)
 
-	# print("X:", X)
-	# print("Labels:", Y)
-	
 	return [X, Y0]
-#-------------------------------------------------------------------------------	
-def read_data_(address, file_name):
+#---------------------------------------------------------------------------------------------------	
+def readData(address, file_name):
 	pass
-#-------------------------------------------------------------------------------	
-def create_observational_data(X,Y0):
-	Y = Y0
-	return [X,Y,Y0]
-#-------------------------------------------------------------------------------	
+#---------------------------------------------------------------------------------------------------
+def syntheticObservationalData(d, m, n, logPolicy): 
+	# this function generates synthetic data as described in the paper "optimal rescriptive trees"
+	# it takes as input d: dimention of the covariate vector, m: the number of treatments, ...
+	# n: the sample size, logPolicy is the offline policy that generated the observational data
+	
+	# np.random.seed(1)
+	X = np.array([[0.0 for i in range(d)] for j in range(n)], dtype = object)
+	for i in range(n):
+		for j in range(d):
+			if(j%2 == 0):
+				X[i,j] = norm.rvs(0, 1)
+			else: 
+				X[i,j] = bernoulli.rvs(0.5)
+
+	#print('X: ', X)
+
+	# define functions used for the outcome variable 
+	def baseline1(x):
+		return x[0] + x[2] + x[4] + x[6] + x[7] + x[8] - 2
+
+	def effect1(x):
+		if(x[0] > 1):
+			return 0
+		else:
+			return -5
+
+	def baseline2(x):
+		return 0
+
+	def effect2(x):
+		if(x[0] > 1 and x[2] > 0 and x[4] > 1 and x[6] > 0):
+			
+			return 8 + 2*x[7]*x[8]
+
+		if((x[0] <= 1 or x[2] <= 0) and (x[4] <= 1 or x[6] <= 0)):
+			
+			return 2*x[7]*x[8]
+
+		if(((x[0] <= 1 or x[2] <= 0) and (x[4] > 1 and x[6] > 0)) or ((x[0] > 1 and x[2] > 0) 
+			and (x[4] <= 1 or x[6] <= 0))):
+			
+			return 4 + 2*x[7]*x[8]
+
+	Y = np.array([[0.0 for i in range(m)] for j in range(n)], dtype = object)
+
+	for j in range(n):
+		Y[j][0] = baseline1(X[j]) + 0.5*effect1(X[j])
+		Y[j][1] = baseline1(X[j]) - 0.5*effect1(X[j])
+
+	#print('Y: ', Y)
+
+	T = np.array([[0.0 for i in range(n)]], dtype = object)
+	for i in range(n):
+		T[0][i] = bernoulli.rvs(math.exp(Y[i][0])/(1 + math.exp(Y[i][0])))
+
+	#print('T: ', T)
+
+	YObs = np.array([[0 for i in range(n)]], dtype = object)
+
+	for i in range(n):
+		YObs[0][i] = Y[i][T[0][i]]
+
+	#print('Observed Y: ', YObs)
+	S = np.concatenate((np.concatenate((X, YObs.transpose()), axis = 1), T.transpose()), axis = 1)
+	SFull = np.concatenate((np.concatenate((X, Y), axis = 1), T.transpose()), axis = 1)
+
+	# split train and test data
+	r = np.random.rand(S.shape[0])
+
+	Train = S[r<0.9, :]
+	Test = SFull[r>=0.9, :]
+
+	return [Train, Test]
+#---------------------------------------------------------------------------------------------------	
+def printInorder(root): 
+    if root != None: 
+        # First recur on left child 
+        printInorder(root.childLeft) 
+  
+        # then print the data of node 
+        print('(feature, value): ', root.theta, root.j)
+  
+        # now recur on right child 
+        printInorder(root.childRight)
+#---------------------------------------------------------------------------------------------------	
+def main_synthetic():
+	d = 10
+	n = 500
+	m = 2
+	doMatching = False
+	res = syntheticObservationalData(d, m, n, [])
+
+	Train = res[0]
+	Test = res[1]
+
+	max_depth = 5
+	min_leaf_number = 0
+	# create the personalization tree
+	pt = tree.personalizationTree(Train, Test, max_depth, min_leaf_number, doMatching)
+
+	print('the personalization tree is: ')
+	printInorder(pt.root)
+
+	value = pt.policyEvaluation(pt.policy)
+	print('Personalization Tree value: ', value)
+
+	value_base = baseline(Train, Test, ['RC'])
+	print('Random Forest value: ', value_base)
+#---------------------------------------------------------------------------------------------------	
+def baseline(Train, Test, methods):
+	T = set(Train[:,-1])
+	m = len(T)
+
+	value = [0 for a in methods]
+	for a in range(len(methods)):
+		if(methods[a] == 'RC'): # regression and compare
+			# random forest
+			RCModels = []
+			for i in range(m):
+				X = Train[Train[:,-1] == i, 0:len(Train[0])-2]
+				Y = Train[Train[:,-1] == i, -2]
+
+				RC = RandomForestRegressor()
+				RC.fit(X, Y)
+
+				RCModels.append(RC)
+
+			for i in range(len(Test)):
+				yhat = [0 for i in range(m)]
+
+				for j in range(m):
+					RC = RCModels[j]
+
+					x = Test[i, 0:len(Train[0])-2]
+					y = Test[i,-2]
+					yhat[j] = RC.predict(x.reshape(1,-1))
+
+				p = np.argmin([yhat])
+				value[a] += Test[i, len(x) + p]/len(Test)
+
+		if(a == 'CF'): # causal forest
+			pass
 
 
+	return value
+#---------------------------------------------------------------------------------------------------	
+def main_real():
+	d = 10
+	n = 10
+	m = 2
+	doMatching = True
+	
+	S = readDate()
 
+	max_depth = 3
+	min_leaf_number = 0
+	# create the personalization tree
+	pt = tree.personalizationTree(S, [], max_depth, min_leaf_number, doMatching)
 
+	print('the personalization tree is: ')
+	printInorder(pt.root)
 
+	value = pt.policyEvaluation(pt.policy)
+	print('value: ', value)
+#---------------------------------------------------------------------------------------------------	
 
 if __name__ == "__main__":
-	[X,Y0] = generate_data_guassian(4, 10, 3)
-	[X,Y,Y0] = create_observational_data(X, Y0)
-
-	
+	main_synthetic()
 
 
---
+++ tree.py
@@ -0,0 +1,240 @@
+# Author: Aida Rahmattalabi
+
+# This is an implementation of Algorithm 1 in the paper 
+# "Recursive Partitioning for Personalization using Observational Data"
+
+import numpy as np
+from numpy.linalg import inv
+from scipy.spatial import distance
+import pandas as pd
+import random 
+import math
+#---------------------------------------------------------------------------------------------------	
+class Node():
+	def __init__(self, S, j, theta): # each node is characterized by a feature (index) and split value.
+		self.j = j
+		self.theta = theta
+
+		self.data = S
+		self.childLeft = None
+		self.childRight = None
+#---------------------------------------------------------------------------------------------------	
+class personalizationTree():
+	def __init__(self, Train, Test, max_depth, min_leaf_number, doMatching):
+		
+		self.Train = Train
+		self.Test = Test
+		self.max_depth = max_depth
+		self.min_leaf_number = min_leaf_number
+
+		self.d = int(len(Train[0]) - 2) 					# number of features
+		T = Train[0:len(Train),-1] 
+		self.m = len(set(T))							# number of treatments
+
+		self.candidateCuts = self.d#int(math.sqrt(self.d))
+		
+		if(doMatching):
+			self.nTest = math.floor(0.2*len(Train))
+			res = self.subsetMatching(S)
+
+			self.Train = res[0]
+			self.Test = res[1]
+			self.root = Node(self.Train, 0,0)
+
+			self.subroutine(self.Train, 0, self.root)
+		else:
+			self.root = Node(self.Train, 0,0)
+
+			self.subroutine(self.Train, 0, self.root)
+	#-----------------------------------------------------------------------------------------------
+	def treeTraversal(self, node, x):
+		if(node.childLeft == None or node.childRight == None):
+			res = []
+
+			T = node.data[:,-1]
+			Y = node.data[:,-2]
+
+			for t in range(self.m):
+				ind = [i for i, x in enumerate(T) if x == t]
+				if(len(ind) != 0):
+					tmp = [Y[i]/len(ind) for i in ind]
+
+					res.append(sum(tmp))
+				else:
+					res.append(math.inf)
+
+			return np.argmin(res)
+		else:
+			j = node.j
+			theta = node.theta
+			
+			if(x[j] <= theta):
+				return self.treeTraversal(node.childLeft, x)
+			else:
+				return self.treeTraversal(node.childRight, x)
+	#-----------------------------------------------------------------------------------------------
+	def policy(self, x):
+		return self.treeTraversal(self.root, x)
+	
+	#-----------------------------------------------------------------------------------------------
+	def subroutine(self, S, cur_depth, node):
+		# count the number of data points in the data 
+		res = []
+		T = S[0:len(S), -1]
+
+		for t in range(self.m):
+			ind = [i for (i, x) in enumerate(T) if x == t]
+			res.append(len(ind))
+
+		if(cur_depth < self.max_depth and min(res) > self.min_leaf_number):
+			IStar = math.inf													# line 4
+			lStar = 0															# line 4
+			jStar = 0															# line 4
+			cuts = list(np.random.randint(0, self.d, self.candidateCuts))		# line 5
+			for l in cuts:  # feature 
+				# sort X along l-th feature 
+				S = sorted(S, key=lambda axis:axis[l], reverse=False)
+				S = np.array(S)
+				
+				T = S[0:len(S),-1]
+				Y = S[0:len(S),len(S[0])-2]
+				X = S[0:len(S),0:len(S[0])-2]
+
+				k_mL = [0 for i in range(self.m)]		# left nodes 
+				S_mL = [0 for i in range(self.m)]		# left nodes
+				k_L = 0									# left nodes
+
+				k_mR = [len([i for (i,x) in enumerate(T) if x == t]) for t in range(self.m)]
+				S_mR = []
+
+				for t in range(self.m):
+					ind = [i for i, x in enumerate(T) if x == t]
+					tmp = sum([Y[i] for i in ind])
+					S_mR.append(tmp)
+
+				k_R = int(len(X))
+
+				for j in range(len(X)-1): # the cut index
+					k_L += 1
+					k_R -= 1
+					t = T[j]
+
+					k_mL[t] += 1
+					k_mR[t] -= 1
+
+					S_mL[t] += Y[t]
+					S_mR[t] -= Y[t]
+
+					if(0 in k_mL and 0 not in k_mR):
+						I = k_R*min([S_mR[i]/k_mR[i] for i in range(self.m)])
+
+					elif(0 in k_mR and 0 not in k_mL):
+						I = k_L*min([S_mL[i]/k_mL[i] for i in range(self.m)])
+					
+					elif(0 in k_mR and 0 in k_mL):
+						I = IStar
+
+					else:
+						I = k_L*min([S_mL[i]/k_mL[i] for i in range(self.m)]) + k_R*min([S_mR[i]/k_mR[i] for i in range(self.m)])
+					
+					k_min = min(min(k_mR), min(k_mL))
+
+					if I < IStar and k_min >= self.min_leaf_number:
+						IStar = I
+						lStar = l
+						jStar = j
+
+
+			if IStar < math.inf:
+				S = sorted(S, key=lambda axis:axis[lStar], reverse=False)
+				S = np.array(S)
+
+				S_L = S[0:jStar+1, 0:len(S[0])]
+				S_R = S[jStar+1:len(S), 0:len(S[0])]
+
+				leftNode = Node(S_L, 0,0)
+				node.childLeft = leftNode
+
+				rightNode = Node(S_R, 0,0)
+				node.childRight = rightNode
+
+				self.subroutine(S_L, cur_depth + 1, leftNode)
+				self.subroutine(S_R, cur_depth + 1, rightNode)
+
+				node.j = lStar
+				node.theta =  0.5*(X[jStar,lStar] + X[jStar+1,lStar]) 
+	#-----------------------------------------------------------------------------------------------
+	def distanceFunction(self):
+		# this function implements Mahalanobis distance between all data points in S. 
+		# it is used withing the sub-matching 
+		d = [[0 for i in range(len(S))] for j in range(len(S))]
+		X = S[0:len(S), 0:len(S[0])-2]
+		Sigma = np.cov(S, rowvar=False)
+
+		return d
+	#-----------------------------------------------------------------------------------------------
+	def subsetMatching(self, S):
+		# this function takes as input a function, i.e., policy and a dataset 
+		# to use for policy evaluation. For policy evaluation, we use sub-matching
+		X = S[0:len(S), 0:len(S[0])-2]
+		T = S[0:len(S), -1]
+		Y = S[0:len(S), -2]
+		
+		# extract a test set
+		testSet = list(np.random.randint(0, len(S), self.nTest))	
+		XTest = X[testSet, :]
+		YTest = [[0 for j in range(self.m)] for i in range(self.nTest)]
+		STest = np.concatenate((XTest, YTest), axis = 1)
+		flag = []  # used to flag the points used for evaluation
+
+		# evaluate the inverse of the covariance matrix
+		Sigma = np.cov(X.astype(float), rowvar=False)
+		Sigmainv = inv(Sigma)
+
+		d = [[0 for j in range(len(X))] for i in range(len(XTest))]
+		for i in range(len(XTest)):
+			for j in range(len(X)):
+				d[i][j] = math.sqrt(np.matmul(np.matmul(XTest[i]-X[j], Sigmainv),
+				 (XTest[i] - X[j]).transpose()))
+
+
+		for j in range(self.nTest):
+			flag.append(testSet[j])
+
+			for m in range(self.m):
+
+				x = S[testSet[j], 0:len(S[0])-2]
+				t = S[testSet[j], -1]
+				y = S[testSet[j], -2]	
+
+				if(m == t):
+					STest[j][self.d+t] = y
+				else:
+					sameT = [i for i, x in enumerate(d[j]) if T[i] == m]
+					i = np.argmin(sameT)
+					y = S[i, -2]	
+					np.append(flag,i)
+
+					Test[j][self.d+m] = y
+
+		Train = np.delete(S, flag, axis = 0)
+
+		return [Train, Test]
+	#-----------------------------------------------------------------------------------------------
+	def policyEvaluation(self, f):
+		# policy value
+		val = 0
+		for i in range(len(self.Test)):
+
+			# find the covariate vector
+			x = self.Test[i,0:self.d]
+			
+			# find the action proposed by policy f
+			p = f(x)
+
+			# evaluate the action
+			val += self.Test[i,self.d + p]
+
+		return val/len(self.Test)
+	#-----------------------------------------------------------------------------------------------
+
